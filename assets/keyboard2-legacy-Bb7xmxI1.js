45b3b62e3b13b19253234bef31785f309b22e4d8
Jolinamata <jolinamata707@gmail.com>
2025-04-02 21:27:01 +0800

    Updates

--- assets/keyboard2-legacy-Bb7xmxI1.js
+++ assets/keyboard2-legacy-Bb7xmxI1.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Cx8KOH72.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-3CuNfMc7.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
