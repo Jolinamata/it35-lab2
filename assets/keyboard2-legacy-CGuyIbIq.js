6ff6774a960dfbcd8c92c984dd2e76440cefdbb9
Junessa19 <bahianjunessa@gmail.com>
2025-05-18 21:57:23 -0800

    Updates

--- assets/keyboard2-legacy-CGuyIbIq.js
+++ assets/keyboard2-legacy-CGuyIbIq.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CHM1xl4S.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-ByB_dJFz.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-DewP3aGP.js
+++ assets/md.transition-legacy-DewP3aGP.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CHM1xl4S.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-ByB_dJFz.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
